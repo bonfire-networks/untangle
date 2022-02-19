19d1f30b1b86bdd38be67b266c8f0bf6e3251604
jjl <james.laver@gmail.com>
2022-02-19 09:19:53 +0100

    v0.1.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,21 @@ defmodule Where.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: [],
+      description: "Logging and inspecting with location information.",
+      homepage_url: "https://github.com/bonfire-networks/where",
+      source_url: "https://github.com/bonfire-networks/where",
+      package: [
+        licenses: ["Apache-2.0"],
+        links: %{
+          "Repository" => "https://github.com/bonfire-networks/where",
+          "Hexdocs" => "https://hexdocs.pm/where",
+        },
+      ],
+      docs: [
+        main: "readme", # The first page to display from the docs 
+        extras: ["README.md"], # extra pages to include
+      ],
+      deps: [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}],
     ]
   end
 
