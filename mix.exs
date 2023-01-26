79e3ae8120bf42c06a0cd1d63f6ea85455c641c1
Mayel de Borniol <git@mayel.space>
2023-01-27 07:32:41 +1300

    Up

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Untangle.MixProject do
   def project do
     [
       app: :untangle,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: "Logging and inspecting with code location information",
