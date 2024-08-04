467db2c32b739156c0e89418a0c781e6711aa94d
Mayel de Borniol <git@mayel.space>
2024-08-04 16:46:40 +0100

    docs

--- lib/untangle.ex
+++ lib/untangle.ex
@@ -176,19 +176,19 @@ defmodule Untangle do
   - an error tuple with the label, if any
   - an error tuple with the passed value otherwise
 
-      iex> error(:value)
+    iex> error(:value)
     ### [error] :value
     {:error, :value}
 
-      iex> error({:error, :value})
+    iex> error({:error, :value})
     ### [error] :value
     {:error, :value}
 
-      iex> error(:value, "with label")
+    iex> error(:value, "with label")
     ### [error] with label: :value
     {:error, "with label"}
 
-      iex> error({:error, :value}, "with label")
+    iex> error({:error, :value}, "with label")
     ### [error] with label: :value
     {:error, "with label"}
   """
